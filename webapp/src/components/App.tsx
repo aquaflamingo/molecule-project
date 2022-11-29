bc2834a2e9ac458a83768ad318a030f8202eb209
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-11-29 15:58:57 -0500

    webapp: Adds typescript basic injectable web3app

---
+++ webapp/src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  // const account = useETHAccounts()[0];
+
+	 return (
+		<div className="Web3App">
+			 <main>
+			</main>		
+		</div>
+	 )
+}
+
+export default App;
---
+++ webapp/src/components/Web3Connector.tsx
@@ -0,0 +1,55 @@
+import { useWeb3React } from '@web3-react/core'
+import injectedConnector from '../connectors/injected'
+
+// Web3Connect is responsible for rendering the initial connect button
+// to initiate Web3 connection using the Web3React hook to communicate with
+// the Web3Provider.
+
+type Props = {
+  children: JSX.Element,
+};
+
+const Web3Connect = ({ children } : Props) => {
+	 const { active, activate } = useWeb3React()
+
+	 // Attempts to activate the Web3 connection using the injectedConnector 
+	 const connectToWeb3 = () => {
+			console.log("Web3Connect: Web3 Activating")
+
+			activate(injectedConnector, undefined, true).catch(err => {
+				 console.log("Web3Connect: Web3 Failed to activate")
+				 console.error(err)
+				alert(`Failed to connect to web3 ${err}`)
+				 debugger
+			})
+	 }
+
+	 console.log("Web3Connect: active is ", active)
+
+	 return active ?
+			<div>{children}</div> :
+			(
+				<PleaseConnectWallet connect={connectToWeb3}/>
+			)
+}
+
+type PleaseConnectWalletProps = {
+	connect : () => void
+}
+
+const PleaseConnectWallet = ({ connect } : PleaseConnectWalletProps) => {
+
+	return (
+		<div> 
+			<h1>Wallet required</h1>
+			<p>This is a Web3 application and requires an injected provider like Metamask</p>
+			<p><strong>Please ensure you are connected to Goerli</strong></p>
+
+			 <button type="button" onClick={() => connect()}>
+					Connect
+			 </button>
+		</div>
+	)
+}
+
+export default Web3Connect
